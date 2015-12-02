1b4b2182b3f1385d8b916b37f3b844fe3cefc00d
Jonas Malaco Filho <jonas@jonasmalaco.com>
2015-12-02 17:05:15 -0200

    Add TODOs to Docker externs

--- src/js/npm/dockerode/Docker.hx
+++ src/js/npm/dockerode/Docker.hx
@@ -15,11 +15,13 @@ extern class Docker {
 	@:selfCall
 	function new(?opts:js.npm.dockerModem.Options);
 
+	// TODO type opts and Readable type paramenter
 	@:overload(function(file:Buffer, opts:Dynamic, callback:Error->String->Void):Void {})
 	@:overload(function(filename:String, opts:Dynamic, callback:Error->Readable<Dynamic>->Void):Void {})
 	@:overload(function(filename:String, opts:Dynamic, callback:Error->String->Void):Void {})
 	function buildImage(file:Buffer, opts:Dynamic, callback:Error->Readable<Dynamic>->Void):Void;
 
+	// TODO type opts
 	function createContainer(opts:Dynamic, callback:Error->Container->Void):Void;
 }
 
